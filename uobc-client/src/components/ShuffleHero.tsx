3f3b545b36c5f77559ffb863d6b0f2078e1fffb4
sam-harri <sharr125@uottawa.ca>
2024-05-08 18:53:13 -0400

    unoptimized image

--- uobc-client/src/components/ShuffleHero.tsx
+++ uobc-client/src/components/ShuffleHero.tsx
@@ -21,7 +21,7 @@ const ShuffleHero = () => {
             <nav className="bg-red-900 text-white py-4">
                 <div className="flex items-center justify-between w-full mx-auto px-5">
                     <div className="flex items-center">
-                        <Image src="https://sam-harri.github.io/uOBC/logouobc.png" alt="uOBC Logo" width={40} height={40} className="rounded-full" />
+                        <Image src="/uOBC/logouobc.png" alt="uOBC Logo" width={40} height={40} className="rounded-full" unoptimized/>
                     </div>
                 </div>
             </nav>
