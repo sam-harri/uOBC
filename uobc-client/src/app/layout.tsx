b41d7acb742dba1127866517d07573b55ca63042
sam-harri <sharr125@uottawa.ca>
2024-05-08 19:11:12 -0400

    icon metdata

--- uobc-client/src/app/layout.tsx
+++ uobc-client/src/app/layout.tsx
@@ -1,25 +1,21 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Head from 'next/head';
-
-const inter = Inter({ subsets: ['latin'] })
+// app/layout.tsx
+import './globals.css';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'uOBC',
-  description: 'Univerity of Ottawa Signup Website',
-}
+    title: 'uOBC',
+    description: 'University of Ottawa Signup Website',
+    icons: {
+        icon: '/favicon.ico', // Path relative to the public directory
+    },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-      <html lang="en">
-          <Head>
-              <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-              <title>Your Website Title</title>
-          </Head>
-          <body>
-              {children}
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
