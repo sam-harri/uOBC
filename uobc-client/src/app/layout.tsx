694e7553be8d8d1a18c32bcb4765e827a7d64e02
sam-harri <sharr125@uottawa.ca>
2024-05-08 19:21:11 -0400

    icon success 2?

--- uobc-client/src/app/layout.tsx
+++ uobc-client/src/app/layout.tsx
@@ -9,16 +9,13 @@ export const metadata: Metadata = {
   title: 'uOBC',
   description: 'Univerity of Ottawa Signup Website',
   icons: {
-    icon: '/favicon.ico', // Path relative to the public directory
+    icon: '/uOBC/favicon.ico', // Path relative to the public directory
 },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
       <html lang="en">
-          <Head>
-              <link rel="icon" href="uOBC/favicon.ico" type="image/x-icon" />
-          </Head>
           <body>
               {children}
           </body>
